6bc1a1f6418cfade2cc67651ee891f2bfd42f28d
consuelita <adelita@operatorfoundation.org>
2020-12-29 14:44:28 -0600

    Update SwiftPCAPLive.swift

--- Sources/SwiftPCAP/SwiftPCAPLive.swift
+++ Sources/SwiftPCAP/SwiftPCAPLive.swift
@@ -26,28 +26,31 @@ extension SwiftPCAP {
     /// - parameter bufferSize: The size of the libpcap input buffer
     /// - parameter snaplen: The libpcap snaplen parameter (packet size to capture)
     ///
-    public init(interface: String, bufferSize: Int32 = 104857600, snaplen: Int32 = 8192) throws {
-      // initialize Live capture parameters
-      self.bufferSize = bufferSize
-      self.snaplen = snaplen
-
-      super.init()
-      
-      // prepare error buffer for libpcap to use
-      let errbuf = UnsafeMutablePointer<Int8>.allocate(capacity: Int(PCAP_ERRBUF_SIZE))
-      
-      // create the pcap_t handle for live capture
-      pcapDevice = pcap_create(interface, errbuf)
-
-      if (pcapDevice == nil) {
-        throw Errors.errorMessage(msg: String(cString: errbuf))
-      }
-
-      // private func to set all live capture options
-      try setOptions()
-      
-      // activate the live capture handle
-      pcap_activate(pcapDevice);
+    public init(interface: String, bufferSize: Int32 = 104857600, snaplen: Int32 = 8192) throws
+    {
+        // initialize Live capture parameters
+        self.bufferSize = bufferSize
+        self.snaplen = snaplen
+        
+        super.init()
+        
+        // prepare error buffer for libpcap to use
+        let errbuf = UnsafeMutablePointer<Int8>.allocate(capacity: Int(PCAP_ERRBUF_SIZE))
+        
+        // create the pcap_t handle for live capture
+        pcapDevice = pcap_create(interface, errbuf)
+        
+        if (pcapDevice == nil) {
+            throw Errors.errorMessage(msg: String(cString: errbuf))
+        }
+        
+        // private func to set all live capture options
+        try setOptions()
+        
+        // activate the live capture handle
+        pcap_activate(pcapDevice)
+        
+        try handleReturnCode(pcap_setnonblock(pcapDevice, 1, nil))
     }
 
     ///
@@ -58,21 +61,10 @@ extension SwiftPCAP {
     private func setOptions() throws {
       // libpcap options
       // currently no error handling, may want to add that
-        print("Calling...")
-        print("pcap_set_buffer_size \(bufferSize)")
       try handleReturnCode(pcap_set_buffer_size(pcapDevice, bufferSize))
-        
-        print("pcap_set_snaplen \(snaplen)")
       try handleReturnCode(pcap_set_snaplen(pcapDevice, snaplen))
-        
-        print("pcap_set_promisc 1")
       try handleReturnCode(pcap_set_promisc(pcapDevice, 1))
-        
-        print("pcap_set_timeout 1")
       try handleReturnCode(pcap_set_timeout(pcapDevice, 1))
-        
-        print("pcap_setnonblock 1")
-      try handleReturnCode(pcap_setnonblock(pcapDevice, 1, nil))
     }
   }
 
